8ab59638ecc9a669c3c641981e4f3c02d647b337
Taejoon Kim <tkim00@stuy.edu>
2019-03-20 16:09:55 -0400

    fixing merge sort

--- Merge.java
+++ Merge.java
@@ -1,6 +1,10 @@
 public class Merge {
   public static void main(String[] args) {
-
+    int[] sample = {38, 27, 43, 3, 9, 82, 10};
+    mergesort(sample);
+    for (int i = 0; i < sample.length; i++) {
+      System.out.print(sample[i]);
+    }
   }
 
   public static void mergesort(int[] data) {
@@ -15,35 +19,59 @@ public class Merge {
     int half = (hi-lo)/2; //this is for the length of the arrays
     int[] temp;
     int[] temp2;
-    int k = 0;//this is just to keep the index of temp independent from that of data
-    if (half%2==0) { //if length of array is odd;this is because lo and hi is inclusive
+    int k;//this is just to keep the index of temp independent from that of data
+    if ((hi-lo)%2==0) { //if length of array is odd;this is because lo and hi is inclusive
       temp = new int[half];
       temp2 = new int[half+1];
+      k = 0;
       for (int i = lo; i < med; i++) {
-        temp[i] = data[i];
+        temp[k] = data[i];
+        k++;
       }
+      k = 0;
       for (int i = med; i <= hi; i++) {
-        temp2[i] = data[i];
+        System.out.print(k);
+        System.out.println(i);
+        temp2[k] = data[i];
+        k++;
       }
     } else { //when it is even
       temp = new int[half+1];
       temp2 = new int[half+1];
+      k = 0;
       for (int i = lo; i <= med; i++) {
-        temp[i] = data[i];
+        temp[k] = data[i];
+        k++;
       }
+      k = 0;
       for (int i = med+1; i <= hi; i++) {
-        temp[i] = data[i];
+        temp2[k] = data[i];
+        k++;
       }
     }
-    mergesortH(temp, lo, med);
-    mergesortH(temp2, med+1, hi);
+    mergesortH(temp, lo, med-1);
+    mergesortH(temp2, med, hi);
     int[] temp3 = new int[hi-lo+1];
-    for (int i = 0; i < hi-lo+1; i+=2) { //the problem is the temporary index should not match the index of data.
-
+    for (int i = 0; i < temp.length; i++) { //problem solved.
+      if (temp[i] < temp2[i]) {
+        temp3[i*2] = temp[i];
+        temp3[i*2+1] = temp2[i];
+      } else {
+        temp3[i*2] = temp2[i];
+        temp3[i*2+1] = temp2[i];
+      }
     }
-  }
-
-  public static void merge() {
-
+    if (temp3.length%2!=0) {//when merging uneven subarrays
+      if (temp2[temp2.length-1] > temp3[temp3.length-2]) {
+        temp3[temp3.length-1] = temp2[temp2.length-1];
+      } else {
+        int biggest = temp3[temp3.length-2];
+        temp3[temp3.length-2] = temp2[temp2.length-1];
+        temp3[temp3.length-1] = biggest;
+      }
+    }
+    if (temp3.length == data.length) {
+      data = temp3;
+    }
   }
 }
